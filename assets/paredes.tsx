6f2085e7da7590a1c0513c0fcc5f227ebecdea98
Héric Moura <heric.m.r@gmail.com>
2025-05-03 00:08:31 -0300

    Updates

---
+++ assets/paredes.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Room_Builder_free_16x16" tilewidth="16" tileheight="16" tilecount="391" columns="17">
+ <editorsettings>
+  <export target="paredes.tsx" format="tsx"/>
+ </editorsettings>
+ <properties>
+  <property name="collides" type="object" value="0"/>
+ </properties>
+ <image source="Room_Builder_free_16x16.png" width="272" height="368"/>
+</tileset>
