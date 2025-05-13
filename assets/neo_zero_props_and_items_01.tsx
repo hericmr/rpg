0c0f8f540ec3fc20fed7ea524903e94af71d6b43
Héric Moura <heric.m.r@gmail.com>
2025-05-12 22:07:59 -0300

    Updates

--- assets/neo_zero_props_and_items_01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="neo_zero_props_and_items_01" tilewidth="16" tileheight="16" tilecount="100" columns="10">
- <image source="neo_zero_props_and_items_01.png" width="160" height="160"/>
-</tileset>
